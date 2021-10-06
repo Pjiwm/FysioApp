d49b2b98d4bd11941bbd6a986872745795f5f6f8
Pjiwm <Pim.Munne@gmail.com>
2021-10-06 16:01:32 +0200

    added enum roles

--- Portal/Attributes/AgeAttribute.cs
+++ Portal/Attributes/AgeAttribute.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Portal.Attributes
 {
-    public class AgeAttribute
+    public class AgeAttribute : ValidationAttribute
     {
+        readonly int _minAge;
+        public AgeAttribute(int minAge)
+        {
+            _minAge = minAge;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime time)
+            {
+                return DateTime.Now.Year - time.Year >= _minAge;
+            }
+
+            return false;
+        }
     }
 }
