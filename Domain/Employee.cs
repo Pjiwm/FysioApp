8fd33ab48e647aff7c003c89589a30d9d420459c
Pjiwm <Pim.Munne@gmail.com>
2021-10-06 21:44:47 +0200

    added employee logic

--- Domain/Employee.cs
+++ Domain/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace Domain
 {
     class Employee
     {
+        [Required]
+        string Name { get; set; }
+        [EmailAddress]
+        string Email { get; set; }
+        Gender Gender { get; set; }
+        [Phone]
+        string PhoneNumber { get; set; }
+        [Key]
+        [Required]
+        // can either be a student or employee ID
+        int ID { get; set; }
+        [Required]
+        EmployeeRole Role { get; set; }
+        // BIG-Number is only for Employees with physical therapist role
+        int BIGNumber { get; set; }
     }
 }
