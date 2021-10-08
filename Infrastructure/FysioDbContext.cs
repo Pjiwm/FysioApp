18b9b702a3b61ce2fca07c75592e56eb1ff161d0
Pjiwm <Pim.Munne@gmail.com>
2021-10-08 14:03:32 +0200

    added DBContext and Patient repository via DB

---
+++ Infrastructure/FysioDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    class FysioDbContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public FysioDbContext(DbContextOptions<FysioDbContext> contextOptions) : base(contextOptions)
+        {
+
+        }
+    }
+}
